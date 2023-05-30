305e62a823bf5c81109f6edf1948e92c0151574c
AlexFlores6651 <106271632+AlexFlores6651@users.noreply.github.com>
2023-05-29 23:47:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express"); //importar express
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const misRutas = require("./routes/rutas");
+
+
+const app = express(); //crear al servidor
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(process.cwd()+"/public/"));
+
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+app.use('/', misRutas);
+
+
+app.listen(port, () => {
+  console.log(`hola servidor ejecucion en http://localhost:${port}`);
+})
+
+
+
+   
+
